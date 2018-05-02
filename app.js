e48e8ef0fb75d918e9e5f5922e560e08dbb6edc3
Arturas Paleicikas <paleicikas@gmail.com>
2018-05-02 11:44:46 +0300

    no message

--- app.js
+++ app.js
@@ -1,42 +1,102 @@
 require('dotenv').config();
 
-const Web3 = require('web3');
-const express = require('express');
-const fs = require('fs');
+const mnemonic = process.env.MNEMONIC || '';
+if (!mnemonic.length) {
+  throw new Error('Mnemonic not set.');
+}
 
-const app = express();
-const obj = JSON.parse(fs.readFileSync('./build/contracts/DesicoCrowdsale.json', 'utf8'));
-
-if (!process.env.ADDRESS_CONTRACT || !process.env.ADDRESS_CONTRACT.length) {
+const contractAddress = process.env.ADDRESS_CONTRACT || '';
+if (!contractAddress.length) {
   throw new Error('Contract address not set.');
 }
+const providerUrl = process.env.PROVIDER || '';
+if (!providerUrl.length) {
+  throw new Error('Provider URL not set.');
+}
+
+const Web3 = require('web3');
+const Tx = require('ethereumjs-tx');
+const CryptoJS = require('crypto-js');
+const express = require('express');
+const fs = require('fs');
+const bip39 = require('bip39');
+const coder = require('web3/lib/solidity/coder');
+var hdkey = require('ethereumjs-wallet/hdkey');
+const app = express();
+
+const obj = JSON.parse(fs.readFileSync('./build/contracts/DesicoCrowdsale.json', 'utf8'));
+
+const hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic));
+const walletHdpath = 'm/44\'/60\'/0\'/0/';
+const addressIndex = 0;
+const numAddresses = 1;
+
+var wallets = {};
+var addresses = [];
+
+for (let i = addressIndex; i < addressIndex + numAddresses; i++) {
+  var wallet = hdwallet.derivePath(walletHdpath + i).getWallet();
+  var addr = '0x' + wallet.getAddress().toString('hex');
+  
+  addresses.push(addr);
+  wallets[addr] = wallet;
+}
+
+const account = addresses[addressIndex];
+const privateKey = wallets[account].getPrivateKey();
 
 if (!process.env.PROVIDER || !process.env.PROVIDER.length) {
   throw new Error('Provider not set.');
 }
 
-console.log('Provider: ' + process.env.PROVIDER);
+const provider = new Web3(new Web3.providers.HttpProvider(providerUrl));
+const ico = provider.eth.contract(obj.abi).at(contractAddress);
+provider.eth.defaultAccount = account; // provider.eth.coinbase;
 
-const provider = new Web3(new Web3.providers.HttpProvider(process.env.PROVIDER));
-const ico = provider.eth.contract(obj.abi).at(process.env.ADDRESS_CONTRACT);
-provider.eth.defaultAccount = provider.eth.coinbase;
+var _sendRawTransaction = function (functionName, types, args, cb) {
+  const fullName = functionName + '(' + types.join() + ')';
+  const signature = CryptoJS.SHA3(fullName, { outputLength: 256 }).toString(CryptoJS.enc.Hex).slice(0, 8);
+  const dataHex = signature + coder.encodeParams(types, args);
+  const data = '0x' + dataHex;
+
+  const nonce = provider.toHex(provider.eth.getTransactionCount(account));
+  const gasPrice = provider.toHex(provider.eth.gasPrice);
+  const gasLimitHex = provider.toHex(600000);
+  const rawTx = {
+    nonce: nonce,
+    gasPrice: gasPrice,
+    gasLimit: gasLimitHex,
+    from: account,
+    to: contractAddress,
+    data: data,
+  };
+
+  const tx = new Tx(rawTx);
+  tx.sign(privateKey);
+
+  const serializedTx = '0x' + tx.serialize().toString('hex');
+
+  provider.eth.sendRawTransaction(serializedTx, cb);
+};
 
 app.post('/whitelist/:address', (req, res) => {
-  try {
-    ico.addToWhitelist(req.params.address);
-    res.send('');
-  } catch (e) {
-    res.status(500).send(e);
-  }
+  _sendRawTransaction('addToWhitelist', ['address'], [req.params.address], function (err, txHash) {
+    if (err) {
+      return res.status(500).send({ 'error': err });
+    };
+
+    res.send(txHash);
+  });
 });
 
 app.delete('/whitelist/:address', (req, res) => {
-  try {
-    ico.removeFromWhitelist(req.params.address);
-    res.send('');
-  } catch (e) {
-    res.status(500).send(e);
-  }
+  _sendRawTransaction('removeFromWhitelist', ['address'], [req.params.address], function (err, txHash) {
+    if (err) {
+      return res.status(500).send({ 'error': err });
+    };
+
+    res.send(txHash);
+  });
 });
 
 app.get('/whitelist/:address', (req, res) => {
